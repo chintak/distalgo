8cf0230b982e6c702ad59c92d779cfe1020c4866
Bo Lin <bo@dreamsphere.org>
2016-02-18 10:54:18 -0500

    Update tests.

--- da/test/test_ast.py
+++ da/test/test_ast.py
@@ -1,13 +1,33 @@
 
-import da.compiler.ui
+from da.compiler.ui import *
 
 prog1 = """
 class P(process):
     def setup():
         pass
 
-    def main():
+    def run():
         --start
         a = 1
 """
 
+ast1 = daast_from_str(prog1)
+print(ast1.body[0].body[0].is_atomic)
+
+prog2 = """
+class P(process):
+    def setup(b):
+        pass
+
+    def run():
+        a = 1
+        while True:
+            sub()
+
+    def sub():
+        --start
+        self.b = 1
+"""
+
+ast2 = daast_from_str(prog2)
+print(ast2.body[0].body[1].is_atomic)
